cd9735285d757cb8fe78669f911fa0f41cb9bf3f
Tom Wu <tom76kimo@yahoo-inc.com>
2017-02-20 18:12:25 +0800

    Add basic files

---
+++ generators/app/index.js
@@ -0,0 +1,41 @@
+'use strict';
+var Generator = require('yeoman-generator');
+var chalk = require('chalk');
+var yosay = require('yosay');
+
+module.exports = Generator.extend({
+  prompting: function () {
+    // Have Yeoman greet the user.
+    this.log(yosay(
+      'Welcome to the wonderful ' + chalk.red('generator-genrator-elm-app') + ' generator!'
+    ));
+
+    var prompts = [{
+      type: 'confirm',
+      name: 'someAnswer',
+      message: 'Would you like to enable this option?',
+      default: true
+    }];
+
+    return this.prompt(prompts).then(function (props) {
+      // To access props later use this.props.someAnswer;
+      this.props = props;
+    }.bind(this));
+  },
+
+  writing: function () {
+    this.fs.copy(
+      this.templatePath('index.html'),
+      this.destinationPath('index.html')
+    );
+
+    this.fs.copy(
+      this.templatePath('src'),
+      this.destinationPath('src')
+    );
+  },
+
+  install: function () {
+    this.installDependencies();
+  }
+});
---
+++ generators/app/templates/src/Main.elm
@@ -0,0 +1,34 @@
+module Main exposing (..)
+
+import Messages exposing (Msg(..))
+import Models exposing (Model, initialModel)
+import Navigation exposing (Location)
+import Players.Commands exposing (fetchAll)
+import Routing exposing (Route)
+import Update exposing (update)
+import View exposing (view)
+
+init : Location -> ( Model, Cmd Msg )
+init location =
+    let
+        currentRoute =
+            Routing.parseLocation location
+    in
+        ( initialModel currentRoute, Cmd.map PlayersMsg fetchAll )
+
+-- SUBSCRIPTIONS
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+-- MAIN
+
+main : Program Never Model Msg
+main =
+  Navigation.program OnLocationChange
+    {
+        init = init,
+        view = view,
+        update = update,
+        subscriptions = subscriptions
+    }
